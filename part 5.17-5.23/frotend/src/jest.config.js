fe196c1246c32e8c75c89ac1608d04648abd066b
gladyreigh <tgladyreigh@gmail.com>
2024-08-20 10:07:16 +0400

    Delete part 5.17-5.23 directory

--- part 5.17-5.23/frotend/src/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  moduleFileExtensions: ["js", "jsx"],
-};
